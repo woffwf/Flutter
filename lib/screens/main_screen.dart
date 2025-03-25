05d54e51a1fa23312b89686bd64cb6add317926b
woffwf <olha.myronovych.ir.2022@lpnu.ua>
2025-03-25 02:07:45 +0200

    final

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -24,41 +24,50 @@ class MainScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
+      body: const DecoratedBox( 
+        decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
               Color.fromARGB(255, 224, 180, 194),
-              Color.fromARGB(255, 222, 172, 190)
+              Color.fromARGB(255, 222, 172, 190),
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Розумне опалення',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 205, 34, 102),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Налаштуйте температуру кімнати:',
-              style: TextStyle(
-                fontSize: 20,
-                color: Color.fromARGB(255, 202, 31, 100),
-              ),
-            ),
-            SizedBox(height: 20),
-            TemperatureControl(),
-          ],
-        ),
+        child: MainContent(), 
       ),
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text(
+          'Розумне опалення',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 205, 34, 102),
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Налаштуйте температуру кімнати:',
+          style: TextStyle(
+            fontSize: 20,
+            color: Color.fromARGB(255, 202, 31, 100),
+          ),
+        ),
+        SizedBox(height: 20),
+        TemperatureControl(),
+      ],
     );
   }
 }
@@ -73,31 +82,35 @@ class TemperatureControl extends StatefulWidget {
 class TemperatureControlState extends State<TemperatureControl> {
   double _temperature = 22.0;
 
- @override
-Widget build(BuildContext context) {
-  return Column(
-    children: [
-      
-      Text(
-        '${_temperature.toStringAsFixed(1)}°C',
-        style: const TextStyle(
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
-          color: Color(0xFFD6236A), 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+        const Text(
+          'Текст за температурою',
+          style: TextStyle(fontSize: 50),
         ),
-      ),
-      Slider(
-        value: _temperature,
-        min: 15.0,
-        max: 30.0,
-        divisions: 30,
-        onChanged: (double newValue) {
-          setState(() {
-            _temperature = newValue;
-          });
-        },
-      ),
-    ],
-  );
-}
+        Text(
+          '${_temperature.toStringAsFixed(1)}°C',
+          style: const TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFD6236A),
+          ),
+        ),
+        Slider(
+          value: _temperature,
+          min: 15.0,
+          max: 30.0,
+          divisions: 30,
+          onChanged: (double newValue) {
+            setState(() {
+              _temperature = newValue;
+            });
+          },
+        ),
+      ],
+    );
+  }
 }

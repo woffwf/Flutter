c05d911b9ba7af8a400303bc5e58e9bc90fd1396
woffwf <olha.myronovych.ir.2022@lpnu.ua>
2025-05-20 20:59:12 +0300

    lab6.3

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'screen/main_screen.dart';
-import 'screen/profile_screen.dart';
-import 'screen/login_screen.dart';
-import 'screen/register_screen.dart';
-import 'screen/qr_scanner_screen.dart';
-import 'screen/saved_qr_screen.dart';
+import 'package:mob/screen/main_screen.dart';
+import 'package:mob/screen/profile_screen.dart';
+import 'package:mob/screen/login_screen.dart';
+import 'package:mob/screen/register_screen.dart';
+import 'package:mob/screen/qr_scanner_screen.dart';
+import 'package:mob/screen/saved_qr_screen.dart';
 
-import 'cubit/auth/auth_cubit.dart';
-import 'cubit/qr/qr_cubit.dart';
-import 'cubit/connection/connection_cubit.dart';
+import 'package:mob/cubit/auth/auth_cubit.dart';
+import 'package:mob/cubit/qr/qr_cubit.dart';
+import 'package:mob/cubit/connection/connection_cubit.dart';
 
 void main() {
   runApp(MyApp());
